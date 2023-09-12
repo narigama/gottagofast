650bdd72628b6dc44c679a684a6dfe28d7cab2c6
David Smith <david@narigama.dev>
2023-09-12 14:17:13 +0100

    ✨ Added eyre and error handler for Axum
    Need to find a nice way to show traceback on error though.

--- fortunes-rs/src/main.rs
+++ fortunes-rs/src/main.rs
@@ -1,9 +1,45 @@
 use std::str::FromStr;
 
-use axum::{extract::State, Json};
+use color_eyre::eyre;
+
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use tokio_postgres::Transaction;
+
+#[derive(Debug, Serialize)]
+pub struct ErrorResponse {
+    status_code: u16,
+    message: String,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Postgres: {0}")]
+    Postgres(#[from] tokio_postgres::Error),
+
+    #[error("Deadpool: {0}")]
+    PoolError(#[from] deadpool_postgres::PoolError),
+
+    #[error("Uncaught Exception")]
+    Eyre(#[from] eyre::Error),
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        tracing::error!("{self}");
+
+        // build a json response and return
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(ErrorResponse {
+                status_code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+                message: self.to_string(),
+            }),
+        )
+            .into_response()
+    }
+}
 
 #[derive(Debug, Parser)]
 pub struct Config {
@@ -11,7 +47,7 @@ pub struct Config {
     pub database_url: String,
 
     #[clap(long, env = "DATABASE_POOL_MAX_SIZE", default_value = "4")]
-    pub database_pool_max_size: i64,
+    pub database_pool_max_size: usize,
 }
 
 #[derive(Debug, Clone)]
@@ -31,12 +67,12 @@ pub struct FortunesListRequest {
 
 #[derive(Debug, Serialize)]
 pub struct FortunesListResponse {
-    pub count: i64,
+    pub count: usize,
     pub items: Vec<Fortune>,
 }
 
-async fn build_pool(config: &Config) -> deadpool_postgres::Pool {
-    let database_url = url::Url::from_str(&config.database_url).unwrap();
+async fn build_pool(config: &Config) -> eyre::Result<deadpool_postgres::Pool> {
+    let database_url = url::Url::from_str(&config.database_url)?;
 
     let mut builder = deadpool_postgres::Config::new();
     builder.application_name = Some(String::from("gottagofast"));
@@ -55,65 +91,81 @@ async fn build_pool(config: &Config) -> deadpool_postgres::Pool {
     builder.user = Some(database_url.username().into());
     builder.password = database_url.password().map(String::from);
 
-    builder
-        .create_pool(
-            Some(deadpool_postgres::Runtime::Tokio1),
-            tokio_postgres::NoTls,
-        )
-        .unwrap()
+    Ok(builder.create_pool(
+        Some(deadpool_postgres::Runtime::Tokio1),
+        tokio_postgres::NoTls,
+    )?)
 }
 
-async fn get_fortunes(db: &Transaction<'_>, count: i64) -> Vec<Fortune> {
-    db.query(
-        "SELECT content FROM fortune ORDER BY random() LIMIT $1",
-        &[&count],
-    )
-    .await
-    .unwrap()
-    .iter()
-    .map(|row| Fortune {
-        content: row.get("content"),
-    })
-    .collect()
+async fn get_fortunes(db: &Transaction<'_>, count: i64) -> eyre::Result<Vec<Fortune>> {
+    Ok(db
+        .query(
+            "SELECT content FROM fortune ORDER BY random() LIMIT $1",
+            &[&count],
+        )
+        .await?
+        .iter()
+        .map(|row| Fortune {
+            content: row.get("content"),
+        })
+        .collect())
 }
 
 async fn fortunes(
     State(state): State<AppState>,
     request: Json<FortunesListRequest>,
-) -> Json<FortunesListResponse> {
-    let mut conn = state.pool.get().await.unwrap();
-    let db = conn.transaction().await.unwrap();
+) -> Result<Json<FortunesListResponse>, Error> {
+    let mut conn = state.pool.get().await?;
+    let db = conn.transaction().await?;
 
     // grab stuff from the database
-    let items = get_fortunes(&db, request.quantity.unwrap_or(5)).await;
+    let items = get_fortunes(&db, request.quantity.unwrap_or(20)).await?;
 
     // ensure you commit here, otherwise when `db` is dropped, it will rollback
-    db.commit().await.unwrap();
+    db.commit().await?;
 
-    Json(FortunesListResponse {
-        count: items.len() as _,
+    Ok(Json(FortunesListResponse {
+        count: items.len(),
         items,
-    })
+    }))
+}
+
+fn install_tracing() {
+    use tracing_error::ErrorLayer;
+    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::{fmt, EnvFilter};
+
+    let fmt_layer = fmt::layer().with_target(false);
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(ErrorLayer::default())
+        .init();
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
     // load .env and parse into Config
     dotenvy::dotenv().ok();
     let config = Config::parse();
-    tracing_subscriber::fmt::init();
 
-    // build a database pool
-    let pool = build_pool(&config).await;
+    // setup tracing and error reporting
+    install_tracing();
+    color_eyre::install()?;
 
-    // check the DB is alive, fail here if not
-    drop(pool.get().await.unwrap());
+    // build a database pool, check the DB is alive, fail here if not
+    let pool = build_pool(&config).await?;
+    drop(pool.get().await?);
 
     // setup app context, all items should be clone-able or wrapped in Arc
     let ctx = AppState { pool };
 
     // build a router and attach endpoint and app state
-    let addr = std::net::SocketAddr::from_str("127.0.0.1:8001").unwrap();
+    let addr = std::net::SocketAddr::from_str("127.0.0.1:8000")?;
     let router = axum::Router::new()
         .route("/fortunes", axum::routing::post(fortunes))
         .with_state(ctx);
@@ -122,5 +174,7 @@ async fn main() {
 
     // run application
     tracing::info!("Listening on {addr}");
-    server.await.unwrap();
+    server.await?;
+
+    Ok(())
 }
